7c7eae1631a683a2bb81932a990620fa139b7260
yangsr2 <112903853+yangsr2@users.noreply.github.com>
2022-12-09 19:19:41 -0600

    Add files via upload

---
+++ WSB.java
@@ -0,0 +1,19 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class WSB {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input = new Scanner(new FileInputStream("comments_2021-1-1_2021-11-29_new.csv"));
+
+        while (input.hasNext()){
+            String string = input.next();
+            string = string.replaceAll( "[^a-zA-Z0-9]", "");
+
+            if (string.contains("https")){
+                string = string.replaceAll(string, " ");
+            }
+            System.out.print(string + " ");
+        }
+    }
+}
