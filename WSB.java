163181f2cf3f0cb6666795b24d2cf579befcd844
yangsr2 <112903853+yangsr2@users.noreply.github.com>
2022-12-12 10:05:47 -0600

    Update WSB.java

--- WSB.java
+++ WSB.java
@@ -4,16 +4,20 @@ import java.io.FileInputStream;
 
 public class WSB {
     public static void main(String[] args) throws FileNotFoundException {
+
         Scanner input = new Scanner(new FileInputStream("comments_2021-1-1_2021-11-29_new.csv"));
 
-        while (input.hasNext()){
+        while (input.hasNext()) {
             String string = input.next();
-            string = string.replaceAll( "[^a-zA-Z0-9]", "");
+            string = string.replaceAll("[^a-zA-Z0-9 .,]|(?<!\\d)[.,]|[.,](?!\\d)", "");
 
-            if (string.contains("https")){
+            if (string.contains("https")) {
                 string = string.replaceAll(string, " ");
             }
             System.out.print(string + " ");
+
         }
     }
+
+
 }
